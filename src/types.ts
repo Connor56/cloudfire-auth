357d54f70c41764acbd7997a7deeea323c751fef
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2025-08-17 21:00:01 +0100

    feat(types): export all the types copied into the library

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+import type {
+  DecodedIdToken,
+  UserRecord,
+  UserIdentifier,
+  GetUsersResult,
+  ListUsersResult,
+  CreateRequest,
+  UpdateRequest,
+  DeleteUsersResult,
+} from "./types/firebase-admin/index.js";
+
+export type {
+  DecodedIdToken,
+  UserRecord,
+  UserIdentifier,
+  GetUsersResult,
+  ListUsersResult,
+  CreateRequest,
+  UpdateRequest,
+  DeleteUsersResult,
+};
