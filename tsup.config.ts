53986815433f6deaf814d3717b9c2f6c5372e8e4
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2025-09-03 10:39:24 +0100

    chore: remove source maps from the build

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"], // ESM only
   dts: true, // generate .d.ts
-  sourcemap: true,
   clean: true,
   target: "esnext", // Cloudflare Workers = modern V8
   minify: false, // optional, but not needed for a lib
