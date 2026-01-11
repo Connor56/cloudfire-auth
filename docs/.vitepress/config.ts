6c75f929915bb0034aab660996250ab725648d57
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2026-01-11 13:03:51 +0000

    docs(config): add the vitepress config file

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vitepress";
+import typedocSidebar from "../api/typedoc-sidebar.json";
+import guidesSidebar from "../guides/guides-sidebar.json";
+
+export default defineConfig({
+  title: "My Package Docs",
+  themeConfig: {
+    sidebar: [
+      { text: "Intro", link: "/" },
+      { text: "Guides", link: "/guides", items: guidesSidebar },
+      {
+        text: "API",
+        items: typedocSidebar,
+      },
+    ],
+    search: {
+      provider: "local",
+    },
+  },
+});
